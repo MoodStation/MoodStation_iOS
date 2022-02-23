7c75befb826f7ecf6fbc349a18afc4b55178ab1f
YongwooKim <keepingitflow@gmail.com>
2022-02-23 20:43:30 +0900

    docs: 서버 저장소 CRUD 인터페이스 구현

---
+++ MoodStation/MoodStation/Utils/Repository.swift
@@ -0,0 +1,15 @@
+//
+//  Repository.swift
+//  MoodStation
+//
+//  Created by Yongwoo Marco on 2022/02/23.
+//
+
+protocol Repository {
+    func create(item: Record, completed: @escaping (Bool) -> Void)
+    func read(id: Int, completed: @escaping (Result<Record, Error>) -> Void)
+    func update(item: Record, completed: @escaping (Bool) -> Void)
+    func delete(item: Record, completed: @escaping (Bool) -> Void)
+    
+    func fetchItems(completed: @escaping (Result<[Record], Error>) -> Void)
+}
