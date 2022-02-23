6dd7a2b804f5f5717d87264a2549e9de4921d318
YongwooKim <keepingitflow@gmail.com>
2022-02-23 19:50:13 +0900

    docs: 프로젝트 생성 Xcode 13.0 Swift Storyboard

---
+++ MoodStation/MoodStation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoodStation
+//
+//  Created by Yongwoo Marco on 2022/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
