5792961e00003d26222b70c242a7342e272c63e3
hogumachu <castle_jun@daum.net>
2022-02-24 21:34:02 +0900

    feat: Font System 세팅

---
+++ MoodStation/MoodStation/Extension/UIFont+.swift
@@ -0,0 +1,28 @@
+//
+//  UIFont+.swift
+//  MoodStation
+//
+//  Created by 홍성준 on 2022/02/24.
+//
+
+import UIKit.UIFont
+
+extension UIFont {
+    
+    // Header
+    static let h1B = UIFont.systemFont(ofSize: 36, weight: .bold)
+    static let h2SB = UIFont.systemFont(ofSize: 20, weight: .semibold)
+    
+    // Button
+    static let button1SB = UIFont.systemFont(ofSize: 16, weight: .semibold)
+    static let button2M = UIFont.systemFont(ofSize: 16, weight: .medium)
+    
+    // Body
+    static let body1R = UIFont.systemFont(ofSize: 16, weight: .regular)
+    static let body0M = UIFont.systemFont(ofSize: 16, weight: .medium)
+    
+    // Caption
+    static let caption1M = UIFont.systemFont(ofSize: 12, weight: .medium)
+    static let caption2R = UIFont.systemFont(ofSize: 12, weight: .regular)
+    
+}
