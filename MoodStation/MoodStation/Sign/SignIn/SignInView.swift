9f2c6228a2307578278119cb9934ad7f39250cd1
hogumachu <castle_jun@daum.net>
2022-03-05 22:00:24 +0900

    feat: SignInView TableView 로 변경

--- MoodStation/MoodStation/Sign/SignIn/SignInView.swift
+++ MoodStation/MoodStation/Sign/SignIn/SignInView.swift
@@ -9,19 +9,23 @@ import UIKit
 import SnapKit
 import Then
 
-protocol SignInViewDelegate: AnyObject {
+protocol SignInViewDelegate: UITableViewDelegate {
     
-    func signInViewDidChangeEmailText(_ view: SignInView)
-    func signInViewDidChangePasswordText(_ view: SignInView)
-    func signInViewDidTapSignIn(_ view: SignInView)
-    
+    func signInViewSignInButtonDidClick(_ view: SignInView)
+    func signInViewfindButtonDidClick(_ view: SignInView)
 }
+
+typealias SignInViewDataSource = UITableViewDataSource
 
 final class SignInView: UIView {
     
-    #warning("Custom TextField 생성해서 TextField 에 적용")
+    weak var delegate: SignInViewDelegate? {
+        didSet { self.tableView.delegate = self.delegate }
+    }
     
-    weak var delegate: SignInViewDelegate?
+    weak var dataSource: SignInViewDataSource? {
+        didSet { self.tableView.dataSource = self.dataSource }
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -35,66 +39,23 @@ final class SignInView: UIView {
     }
     
     private func setupLayout() {
-        self.addSubview(self.titleLabel)
-        self.titleLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(24)
-            make.leading.trailing.equalToSuperview().inset(24)
-            make.height.equalTo(47)
-        }
-        
-        self.addSubview(self.subTitleLabel)
-        self.subTitleLabel.snp.makeConstraints { make in
-            make.top.equalTo(self.titleLabel.snp.bottom).offset(14)
-            make.leading.trailing.equalToSuperview().inset(24)
-            make.height.equalTo(42)
-        }
-        
-        self.addSubview(self.emailLabel)
-        self.emailLabel.snp.makeConstraints { make in
-            make.top.equalTo(self.subTitleLabel.snp.bottom).offset(70)
-            make.leading.trailing.equalToSuperview().inset(24)
-            make.height.equalTo(21)
-        }
-        
-        self.addSubview(self.emailTextField)
-        self.emailTextField.snp.makeConstraints { make in
-            make.top.equalTo(self.emailLabel.snp.bottom).offset(14)
-            make.leading.trailing.equalToSuperview().inset(24)
-            make.height.equalTo(55)
-        }
-        
-        self.addSubview(self.passwordLabel)
-        self.passwordLabel.snp.makeConstraints { make in
-            make.top.equalTo(self.emailTextField.snp.bottom).offset(36)
-            make.leading.trailing.equalToSuperview().inset(24)
-            make.height.equalTo(21)
-        }
-        
-        self.addSubview(self.passwordTextField)
-        self.passwordTextField.snp.makeConstraints { make in
-            make.top.equalTo(self.passwordLabel.snp.bottom).offset(14)
-            make.leading.trailing.equalToSuperview().inset(24)
-            make.height.equalTo(55)
-        }
-        
-        self.addSubview(self.descriptionLabel)
-        self.descriptionLabel.snp.makeConstraints { make in
-            make.top.equalTo(self.passwordTextField.snp.bottom).offset(14)
-            make.leading.trailing.equalToSuperview().inset(24)
+        self.addSubview(self.findButton)
+        self.findButton.snp.makeConstraints { make in
             make.height.equalTo(16)
+            make.bottom.equalToSuperview()
+            make.centerX.equalToSuperview()
         }
         
         self.addSubview(self.signInButton)
         self.signInButton.snp.makeConstraints { make in
-            make.top.equalTo(self.passwordTextField.snp.bottom).offset(80)
             make.leading.trailing.equalToSuperview().inset(24)
             make.height.equalTo(56)
+            make.bottom.equalTo(self.findButton.snp.top).offset(-11)
         }
-        
-        self.addSubview(self.findButton)
-        self.findButton.snp.makeConstraints { make in
-            make.top.equalTo(self.signInButton.snp.bottom).offset(11)
-            make.centerX.equalToSuperview()
+        self.addSubview(self.tableView)
+        self.tableView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(self.signInButton.snp.top)//.offset(-20)
         }
     }
     
@@ -103,91 +64,39 @@ final class SignInView: UIView {
             $0.backgroundColor = .customBlack
         }
         
-        self.titleLabel.do {
-            $0.text = "로그인"
-            $0.font = .h1B
-            $0.textColor = .customWhtie
-        }
-        
-        self.subTitleLabel.do {
-            $0.text = "~에 로그인하고\n기록을 이어나가 보세요."
-            $0.numberOfLines = 2
-            $0.font = .body1R
-            $0.textColor = .customWhtie
-        }
-        
-        self.emailLabel.do {
-            $0.text = "이메일"
-            $0.font = .body0M
-            $0.textColor = .customWhtie
-        }
-        
-        self.emailTextField.do {
-            $0.placeholder = "이메일을 입력해 주세요."
-            $0.backgroundColor = .gray06
-            $0.layer.cornerRadius = 10
-            $0.addTarget(self, action: #selector(emailTextFieldDidChangeText(_:)), for: .editingChanged)
-        }
-        
-        self.passwordLabel.do {
-            $0.text = "비밀번호"
-            $0.font = .body0M
-            $0.textColor = .customWhtie
-        }
-        
-        self.passwordTextField.do {
-            $0.placeholder = "비밀번호를 입력해 주세요."
-            $0.backgroundColor = .gray06
-            $0.layer.cornerRadius = 10
-            $0.addTarget(self, action: #selector(passwordTextFieldDidChangeText(_:)), for: .editingChanged)
-        }
-        
-        self.descriptionLabel.do {
-            $0.text = "이메일 또는 비밀번호를 확인해 주세요." // 나중에 변경될 수 있을 듯
-            $0.font = .caption1M
-            $0.textColor = .error
+        self.tableView.do {
+            $0.separatorStyle = .none
+            $0.backgroundColor = .customBlack
+            $0.register(TextOnlyTableViewCell.self)
+            $0.register(TextFieldOnlyTableViewCell.self)
         }
         
         self.signInButton.do {
             $0.setTitle("로그인", for: .normal)
-            $0.setTitleColor(.customWhtie, for: .normal)
+            $0.titleLabel?.font = .button1SB
             $0.backgroundColor = .main
             $0.layer.cornerRadius = 10
-            $0.addTarget(self, action: #selector(signInButtonDidTap(_:)), for: .touchUpInside)
+            $0.addTarget(self, action: #selector(signInButtonClicked(_:)), for: .touchUpInside)
         }
         
         self.findButton.do {
             $0.setTitle("이메일/비밀번호 찾기", for: .normal)
-            $0.setTitleColor(.gray02, for: .normal)
             $0.titleLabel?.font = .caption2R
+            $0.setTitleColor(.gray02, for: .normal)
+            $0.addTarget(self, action: #selector(findButtonClicked(_:)), for: .touchUpInside)
         }
     }
     
-    @objc private func emailTextFieldDidChangeText(_ sender: UITextField) {
-        self.delegate?.signInViewDidChangeEmailText(self)
+    @objc private func signInButtonClicked(_ sender: UIButton) {
+        self.delegate?.signInViewSignInButtonDidClick(self)
     }
     
-    @objc private func passwordTextFieldDidChangeText(_ sender: UITextField) {
-        self.delegate?.signInViewDidChangePasswordText(self)
+    @objc private func findButtonClicked(_ sender: UIButton) {
+        self.delegate?.signInViewfindButtonDidClick(self)
     }
     
-    @objc private func signInButtonDidTap(_ sender: UIButton) {
-        self.delegate?.signInViewDidTapSignIn(self)
-    }
-    
-    private let titleLabel = UILabel(frame: .zero)
-    private let subTitleLabel = UILabel(frame: .zero)
-    
-    private let emailLabel = UILabel(frame: .zero)
-    private let emailTextField = UITextField(frame: .zero)
-    
-    private let passwordLabel = UILabel(frame: .zero)
-    private let passwordTextField = UITextField(frame: .zero)
-    
-    private let descriptionLabel = UILabel(frame: .zero)
-    
+    private let tableView = UITableView(frame: .zero)
     private let signInButton = UIButton(frame: .zero)
-    
     private let findButton = UIButton(frame: .zero)
     
 }
