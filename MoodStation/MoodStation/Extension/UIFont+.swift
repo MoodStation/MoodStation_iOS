f70426fee6015697d1ff4a125295eebba390f29f
YongwooKim <keepingitflow@gmail.com>
2022-03-09 19:21:23 +0900

    style: UIFont+ 추가

--- MoodStation/MoodStation/Extension/UIFont+.swift
+++ MoodStation/MoodStation/Extension/UIFont+.swift
@@ -12,15 +12,19 @@ extension UIFont {
     // Header
     static let h1B = UIFont.systemFont(ofSize: 36, weight: .bold)
     static let h2SB = UIFont.systemFont(ofSize: 20, weight: .semibold)
+    static let h3M = UIFont.systemFont(ofSize: 20, weight: .medium)
+    static let h4SB = UIFont.systemFont(ofSize: 18, weight: .semibold)
+    static let h5SB = UIFont.systemFont(ofSize: 16, weight: .semibold)
+    
     
     // Button
     static let button1SB = UIFont.systemFont(ofSize: 16, weight: .semibold)
     static let button2M = UIFont.systemFont(ofSize: 16, weight: .medium)
     
     // Body
-    static let body1R = UIFont.systemFont(ofSize: 16, weight: .regular)
     static let body0M = UIFont.systemFont(ofSize: 16, weight: .medium)
-    
+    static let body1R = UIFont.systemFont(ofSize: 16, weight: .regular)
+    static let body2M = UIFont.systemFont(ofSize: 14, weight: .medium)
     static let body3R = UIFont.systemFont(ofSize: 14, weight: .regular)
     
     // Caption
