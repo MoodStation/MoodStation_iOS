d8ec4d4f5d93e0ff5de9f70488927a0375343888
YongwooKim <keepingitflow@gmail.com>
2022-03-23 21:12:50 +0900

    docs: UserInfo 모델 분리

---
+++ MoodStation/MoodStation/Model/UserInfo.swift
@@ -0,0 +1,12 @@
+//
+//  UserInfo.swift
+//  MoodStation
+//
+//  Created by Yongwoo Marco on 2022/03/23.
+//
+
+struct UserInfo {
+    let name: String
+    let email: String
+    let userImagePath: String?
+}
