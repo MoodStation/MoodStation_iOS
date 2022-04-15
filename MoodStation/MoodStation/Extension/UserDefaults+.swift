7e4c60f1baf6042f42c2f0467d8d722770f9eb20
YongwooKim <keepingitflow@gmail.com>
2022-04-15 21:38:47 +0900

    feat: 구조체 형태의 UserDefault Value set, get 방식 추가

--- MoodStation/MoodStation/Extension/UserDefaults+.swift
+++ MoodStation/MoodStation/Extension/UserDefaults+.swift
@@ -22,4 +22,27 @@ extension UserDefaults {
     func setValue(_ value: Any?, key: UserDefaults.Key) {
         self.setValue(value, forKey: key.rawValue)
     }
+    
+    func setObject<Object>(_ object: Object, forKey: String) where Object: Encodable {
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(object)
+            set(data, forKey: forKey)
+        } catch {
+            print("\(#function) - UserDefault set 실패")
+        }
+    }
+    
+    func getObject<Object>(forKey: String, castTo type: Object.Type) -> Object? where Object: Decodable {
+        guard let data = data(forKey: forKey) else {
+            return nil
+        }
+        let decoder = JSONDecoder()
+        do {
+            let object = try decoder.decode(type, from: data)
+            return object
+        } catch {
+            return nil
+        }
+    }
 }
